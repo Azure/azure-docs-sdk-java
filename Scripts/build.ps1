1f9aaca4c826d01230c8df7c02b2c73851ca7684
xuzho <xuzho@microsoft.com>
2016-10-10 12:20:54 +0800

    use new structure

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -2,11 +2,11 @@
 #
 # |-azure-docs-sdk-java
 #    |
+#    |--code2yaml.json
 #    |--Scripts
 #       |
 #       |--build.ps1
 #       |--code2yaml.zip
-#       |--code2yaml.json
 function Unzip 
 { 
      param([string]$zipfile, [string]$outpath) 
@@ -35,30 +35,21 @@ $ErrorActionPreference = 'Stop'
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptHome = Split-Path $scriptPath
 $scriptParent = Split-Path $scriptHome
-$api = "api"
+$apiref = "api-ref"
 $code2yamlZip = "code2yaml.zip"
 $code2yaml = "code2yaml"
-$obj = "obj"
-md -Force $scriptHome\$obj
-Push-Location $scriptHome\$obj
+$src = "src"
+Push-Location $scriptParent\$src
 
-# unzip code2yaml.zip to obj folder
+# unzip code2yaml.zip to src folder
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 if (Test-Path $code2yaml){
     Remove-Item $code2yaml\* -recurse -Force
 }
-Unzip $scriptHome\$code2yamlZip $scriptHome\$obj\$code2yaml
-
-# clone or pull latest sdk code
-
-CloneOrPull "https://github.com/Azure/azure-sdk-for-java.git" "azure-sdk-for-java"
-if ($lastexitcode -ne 0)
-{
-    exit $lastexitcode
-}
+Unzip $scriptHome\$code2yamlZip $scriptParent\$src\$code2yaml
 
 # update config
-$config = Get-Content $scriptHome\code2yaml.json -Raw | ConvertFrom-Json
+$config = Get-Content $scriptParent\code2yaml.json -Raw | ConvertFrom-Json
 $config.output_path = "_javadocs"
 $config | ConvertTo-Json | Out-File "code2yaml.json" -Force
 
@@ -70,7 +61,7 @@ if ($lastexitcode -ne 0)
     exit $lastexitcode
 }
 # update to api folder
-Remove-Item $scriptParent\$api -recurse
-Copy-Item -Path "_javadocs\*" -Destination $scriptParent\$api -Force
+Remove-Item $scriptParent\$apiref -recurse
+Copy-Item -Path "_javadocs\*" -Destination $scriptParent\$apiref -Force
 
 Pop-Location
